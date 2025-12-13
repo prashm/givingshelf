094d6efc08d1f55225ab726cb7f11b3648667cc4
Prashant Mokkarala <prashm@gmail.com>
2025-12-13 00:47:08 -0700

    Made the following changes to ensure so that the Docker build succeeds: Sprockets only processes ActiveAdmin's SCSS files Propshaft handles main.css and application.css without SassC Modern CSS syntax is preserved

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,9 +1,5 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
-// Only link specific CSS files, not all CSS files (to avoid SassC processing modern CSS syntax)
-// Note: application.css comes from webpack, application.scss is for dartsass-rails but we use application.css
-//= link main.css
-//= link application.css
+// Only link ActiveAdmin assets - main.css and application.css are handled by Propshaft
 //= link active_admin.css
 //= link active_admin.js
-
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,9 +6,13 @@ if defined?(Sprockets)
   # Version of your assets, change this if you want to expire all your assets.
   Rails.application.config.assets.version = "1.0"
 
-  # Add additional assets to the asset load path.
-  # Rails.application.config.assets.paths << Emoji.images_path
-
   # Precompile ActiveAdmin assets
   Rails.application.config.assets.precompile += %w[active_admin.css active_admin.js]
+
+  # Configure Sprockets to not process plain CSS files (only SCSS)
+  # This prevents SassC from trying to parse modern CSS syntax in CSS files
+  Rails.application.config.assets.configure do |env|
+    # Unregister the CSS processor that uses SassC
+    env.unregister_preprocessor("text/css", Sprockets::SassProcessor) rescue nil
+  end
 end
