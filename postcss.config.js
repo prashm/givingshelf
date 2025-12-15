18bd3308ad913663679d657f7f1048440b23fb05
Prashant Mokkarala <prashm@gmail.com>
2025-12-14 21:27:01 -0700

    Restore SassC workaround while keeping lean PostCSS config

--- postcss.config.js
+++ postcss.config.js
@@ -3,5 +3,13 @@ module.exports = {
     require('@tailwindcss/postcss'),
     require('postcss-media-minmax'),
     require('autoprefixer'),
+    {
+      postcssPlugin: 'fix-sass-rgb',
+      AtRule(rule) {
+        if (rule.name === 'supports' && rule.params.includes('rgb(from red r g b)') && !rule.params.includes('sass-safe-rgb')) {
+          rule.params = rule.params.replace('rgb(from red r g b)', 'sass-safe-rgb(from red r g b)');
+        }
+      }
+    }
   ],
 }
