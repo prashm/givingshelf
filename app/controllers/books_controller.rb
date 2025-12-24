778d3c77877aead3656f4c56e5d9f9884fbbec4c
Prashant Mokkarala <prashm@gmail.com>
2025-12-23 18:51:52 -0700

    per book json-ld in the header for google crawler to scan through.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,10 @@ class BooksController < ApplicationController
   allow_unauthenticated_access only: [ :index ]
 
   def index
+    # Detect if this is a book detail page (/books/:id)
+    # Extract book ID from the path for SEO meta tags
+    @book = extract_book_from_path if request.format.html?
+
     respond_to do |format|
       format.html do
         # For HTML requests, render the React app
@@ -12,5 +16,16 @@ class BooksController < ApplicationController
         render json: { message: "Please use /api/books for JSON requests" }, status: :not_acceptable
       end
     end
+  end
+
+  private
+
+  def extract_book_from_path
+    # Match /books/:id pattern
+    match = request.path.match(%r{^/books/(\d+)$})
+    return nil unless match
+
+    book_id = match[1].to_i
+    Book.available.find_by(id: book_id)
   end
 end
