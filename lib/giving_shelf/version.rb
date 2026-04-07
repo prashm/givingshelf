28084f51910cc5ec714b37466d91e4706b6d4a40
Prashant Mokkarala <prashm@gmail.com>
2026-04-07 12:59:23 -0600

    version updated

--- lib/giving_shelf/version.rb
+++ lib/giving_shelf/version.rb
@@ -1,7 +1,7 @@
 module GivingShelf
   module Version
     COMPANY_NAME = "SimplifAI LLC"
-    APP_VERSION = "2.1.7"
+    APP_VERSION = "2.1.8"
     COPYRIGHT_START_YEAR = 2025
 
     def self.copyright_end_year
